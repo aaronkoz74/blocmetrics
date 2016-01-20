05e1dbc4485d07cb10d0a4b39369c2541efaf1c3
aaronkoz74 <aaronkoz74@gmail.com>
2016-01-20 11:43:27 -0500

    Register application and remove

--- app/controllers/registered_applications_controller.rb
+++ app/controllers/registered_applications_controller.rb
@@ -1,35 +1,54 @@
 class RegisteredApplicationsController < ApplicationController
-  def index
-    @applications = RegisteredApplication.all
-  end
+  before_action :set_user
+  before_action :authorize_user, only: [:destroy]
 
   def show
     @application = RegisteredApplication.find(params[:id])
   end
 
   def new
-    @user = User.find(params[:user_id])
     @application = RegisteredApplication.new
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @application = @user.registered_application.build(app_params)
+    @application = @user.registered_applications.new(app_params)
+    @application.name.capitalize!
+    @application.save
 
     if @application.save
-      flash[:notice] = "Registered application was saved."
-      redirect_to user_path(@user)
+      redirect_to user_path(@user), notice: "#{@application.name} has been registered."
     else
-      #
+      flash.now[:alert] = "There was an error registering the application.  Please try again."
+      render :new
     end
   end
 
   def destroy
+    @application = @user.registered_applications.find(params[:id])
+
+    if @application.destroy
+      flash[:notice] = "#{@application.name} has been removed from registry list."
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "Application could not be de-registered. Please try again."
+    end
   end
 
   private
 
   def app_params
-    params.require(@application).permit(:name, :url)
+    params.require(:registered_application).permit(:name, :url)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def authorize_user
+    registered_application = RegisteredApplication.find(params[:id])
+    unless current_user == registered_application.user
+      flash[:alert] = "You do not have permission to remove this application."
+      redirect_to [registered_application.user, registered_application]
+    end
   end
 end
